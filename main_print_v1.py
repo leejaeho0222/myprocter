4ecf4fbf868fa3347c7a53d8413c9cf4bf82db4c
leejaeho0222 <zxc038023@gmail.com>
2025-09-30 10:51:16 +0900

    second commit

---
+++ main_print_v1.py
@@ -0,0 +1,17 @@
+# 변수 선언
+name = "lee-jae-ho"
+age = 25
+score = 95.5
+
+# 1. 기본 출력
+print("--- My Profile ---")
+
+# 2. 여러 값 출력
+print("Name:", name, "Age:", age, "Score:", score)
+
+# 3. f-string (가장 많이 쓰임)
+print(f"My name is {name}.")
+print(f"I am {age} years old and my score is {score}.")
+
+# 4. f-string 내 계산식/함수 사용
+print(f"Next year, I will be {age + 1} years old.")
